6b61f28ed9cdec789554dec39f00a050fc5e7a92
Browniee <68281832+Browdevep@users.noreply.github.com>
2023-02-21 22:19:06 +0000

    prompt

--- learning.js
+++ learning.js
@@ -1,3 +1,10 @@
 import { basename } from "node:path";
+console.log("viewing", basename(import.meta.url));
 
-console.log("file is",basename(import.meta.url));
+process.stdout.write("write your name ");
+
+process.stdin.on("data", (data) => {
+    const name = data.toString().trim();
+    console.log("your name is", name);
+    process.exit();
+});
