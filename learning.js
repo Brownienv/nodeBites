71830550324df15596547ee20fdc5c3bfe1f0bb8
Browniee <68281832+Browdevep@users.noreply.github.com>
2023-02-22 01:15:30 +0000

    newline

--- learning.js
+++ learning.js
@@ -9,5 +9,6 @@ const readline = createInterface(process.stdin, process.stdout);
 readline.question("write your name ", (name) => {
     console.log("your name is", name);
     readline.close();
+    
     writeSome("testing.py");
 });
