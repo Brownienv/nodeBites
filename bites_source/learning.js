27ca010aca51a614c669c8212e1c25d2b5333d21
Browniee <68281832+Browdevep@users.noreply.github.com>
2023-02-24 05:19:54 +0000

    organizing

--- bites_source/learning.js
+++ bites_source/learning.js
@@ -1,6 +1,5 @@
 import { basename } from "node:path";
 import { createInterface } from "node:readline";
-import { writeSome } from "./exporting.js";
 
 console.log("viewing", basename(import.meta.url));
 
@@ -9,6 +8,4 @@ const readline = createInterface(process.stdin, process.stdout);
 readline.question("write your name ", (name) => {
     console.log("your name is", name);
     readline.close();
-    
-    writeSome("testing.py");
 });
