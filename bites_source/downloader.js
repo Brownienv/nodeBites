a5d59b9b682835fb14e2befea34e49e5f7637ba4
Browniee <68281832+Browdevep@users.noreply.github.com>
2023-03-02 23:53:46 +0000

    downloader

--- bites_source/downloader.js
+++ bites_source/downloader.js
@@ -6,11 +6,7 @@ export const download = (filename) => {
     const readline = createInterface(process.stdin, process.stdout);
 
     readline.question("enter video url ", (url) => {
-        const stream = ytdl(url, {
-            filter: "audioonly",
-            quality: "highestaudio",
-        });
-
+        const stream = ytdl(url, { filter: "audioonly" });
         stream.pipe(createWriteStream(filename));
     });
 };
